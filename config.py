b3896cc8418f941d16fcca1a0c9031f59b37ed21
Athe-kunal <athekunal@gmail.com>
2023-11-30 02:00:51 -0500

    adding vectordb data

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 SAVE_FOLDER = "movie_reviews_link"
-# MONGODB_CLUSTER = "mongodb+srv://imdb_adt:imdb_adt@cluster0.rcvxgzc.mongodb.net/"
-MONGODB_CLUSTER = "mongodb+srv://astmohap:athe@astmohap.jojxwas.mongodb.net/"
+MONGODB_CLUSTER = "mongodb+srv://imdb_adt:imdb_adt@cluster0.rcvxgzc.mongodb.net/"
+# MONGODB_CLUSTER = "mongodb+srv://astmohap:athe@astmohap.jojxwas.mongodb.net/"
 DATABASE_NAME = "Moviedatabase"
 COLLECTION_NAME = "moviedata"
 WIKIPEDIA_FOLDER = "wikipedia_data"
