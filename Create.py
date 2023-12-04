2198cc94950924c2794200cf2476a1bb744a1006
Athe-kunal <athekunal@gmail.com>
2023-12-04 17:49:05 -0500

    demo video commit

--- Create.py
+++ Create.py
@@ -8,29 +8,28 @@ import subprocess
 
 st.title("Movie Database")
 
-val = st.text_input("Provie a movie or TV series along with year")
+st.write("This database performs CRUD applications for movie reviews and LLM for question-answering")
+val = st.text_input("Provide a movie or TV series along with year")
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
 
 def click_button():
     st.session_state.clicked = True
 
-st.button("Start the pipeline",help="It will submit the reviews that you selected to the database",on_click=click_button)
-st.write(val)
-print(type(val))
-if val!="":
-    start = time.time()
-    st.write("Started scraping data")
-    # subprocess.run(["python","main.py","--movie_name=","Parasite 2019"])
-    scrape_data(val)
+# st.button("Start the pipeline",help="It will submit the reviews that you selected to the database",on_click=click_button)
+# if val!="":
+#     start = time.time()
+#     st.write("Started scraping data")
+#     # scrape_data(val)
 
-    st.write(f"The scraping took {round(time.time()-start,2)} seconds")
+#     # st.write(f"The scraping took {round(time.time()-start,2)} seconds")
+#     st.write(f"The scraping took {458.72} seconds")
 
-    st.write("Inserting the data to database and vector database")
+#     st.write("Inserting the data to database and vector database")
 
-    start = time.time()
-    main_insert()
-    insert_main()
-    st.write(f"The insertion took {round(time.time()-start,2)} seconds")
+#     start = time.time()
+#     main_insert()
+#     insert_main()
+#     st.write(f"The insertion took {round(time.time()-start,2)} seconds")
 
-    st.write("Your database is ready to be used")
+#     st.write("Your database is ready to be used")
